0858284da4c9172027e64406cc65c3bb6cfe8025
cezardlo <cdelao@miners.utep.edu>
2025-04-06 23:08:42 -0600

    1

--- code_7.py
+++ code_7.py
@@ -1,8 +1,25 @@
 import pandas as pd
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
-#If any of this libraries is missing from your computer. Please install them using pip.
+import seaborn as sns
+import numpy as np
 
 filename = 'Flight_Delays_2018.csv'
+df=pd.read_csv(filename)
+df.describe()
+print(df.head())
 
-#ARR_DELAY is the column name that should be used as dependent variable (Y).
+top_airlines = df['OP_CARRIER_NAME'].value_counts().head(5)
+print(top_airlines)
+
+Y = df['ARR_DELAY']
+X = df[['DEP_DELAY', 'CARRIER_DELAY', 'LATE_AIRCRAFT_DELAY', 'WEATHER_DELAY']]
+X = sm.add_constant(X)  # Adds the intercept to the regression
+model = sm.OLS(Y, X).fit()  # Fit the OLS regression model
+print(model.params)  # Show coefficients for each variable including intercept
+fig, ax = plt.subplots()
+fig = sm.graphics.plot_fit(model, 1, ax=ax)  
+plt.title("OLS Fit: DEP_DELAY vs ARR_DELAY")
+plt.xlabel("Departure Delay")
+plt.ylabel("Arrival Delay")
+plt.show()
